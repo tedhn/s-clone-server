8406b36ded1d6c8ea9004ba784b5f8f7ef5745d5
heinhtet naing <6927125-hhn186@users.noreply.gitlab.com>
2022-09-19 16:01:30 +0630

    3c

--- index.js
+++ index.js
@@ -22,49 +22,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-	const { code } = req.body;
+	
+	res.send('aaaaaaaaaaaaaaaaaa login')
 
-	console.log(code);
 
-	try {
-		const data = await spotifyApi.authorizationCodeGrant(code);
-		console.log(data);
-		spotifyApi.setAccessToken(data.body["access_token"]);
-		spotifyApi.setRefreshToken(data.body["refresh_token"]);
-
-		spotifyApi.getMe().then((data) => {
-			res.json(data.body);
-		});
-	} catch (e) {
-		console.log(e);
-	}
-	// try {
-	//   if (!authorized) {
-	//     authorized = true;
-	//     spotifyApi.authorizationCodeGrant(code).then((data) => {
-	//       // console.log("The token expires in " + data.body["expires_in"]);
-	//       // console.log("The access token is " + data.body["access_token"]);
-	//       // console.log("The refresh token is " + data.body["refresh_token"]);
-
-	//       // // Set the access token on the API object to use it in later calls
-	//       spotifyApi.setAccessToken(data.body["access_token"]);
-	//       spotifyApi.setRefreshToken(data.body["refresh_token"]);
-
-	//       console.log(data.body["access_token"]);
-
-	//       spotifyApi.getMe().then((data) => {
-	//         res.json(data.body);
-	//       });
-	//     });
-	//   } else {
-	//     spotifyApi.getMe().then((data) => {
-	//       res.json(data.body);
-	//     });
-	//   }
-	// } catch (e) {
-	//   console.log(e);
-	//   if (e.statusCode === 401) refreshToken();
-	// }
 });
 
 app.listen(port, () => {
